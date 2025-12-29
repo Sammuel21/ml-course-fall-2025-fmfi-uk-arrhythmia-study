8496faf52cb6121ac00a58930098ba73a8951f1b
Samuel Gulas <gulas16@uniba.sk>
2025-12-29 20:28:32 +0100

    add progresss to preprocessing

--- configs/constants.py
+++ configs/constants.py
@@ -2,3 +2,7 @@
 
 DATA_DIR_FOLDER = 'data/ecg-arrhythmia/'
 DATA_DIR = 'WFDBRecords'
+
+# train test split
+TTS_SEED = 21
+TEST_SIZE = 0.10
---
+++ scripts/features.py
@@ -0,0 +1,5 @@
+# Feature extraction
+
+# ML pipeline
+
+# DL pipeline
